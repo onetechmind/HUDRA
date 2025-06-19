51844647dc39d0780f573c45aa5aa4ca88bd90c4
Starman <lance@onetechmind.com>
2025-06-18 23:01:29 -0700

    Update with rounded mica UI.

--- HUDRA/MainWindow.xaml.cs
+++ HUDRA/MainWindow.xaml.cs
@@ -1,34 +1,22 @@
 using HUDRA.Services;
+using Microsoft.UI;
+using Microsoft.UI.Composition.SystemBackdrops;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using WinRT;
+using WinRT.Interop; // Required for WindowNative interop
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace HUDRA
 {
     public sealed partial class MainWindow : Window, INotifyPropertyChanged
     {
-
-        // This is the event the UI system will listen for.
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        // This is a helper method to raise the event.
-        // The [CallerMemberName] attribute automatically fills in the name of the property that called it.
         private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
@@ -43,40 +31,115 @@ namespace HUDRA
                 if (_currentTdpDisplayText != value)
                 {
                     _currentTdpDisplayText = value;
-                    OnPropertyChanged(); // This tells the UI to update itself!
+                    OnPropertyChanged();
                 }
             }
         }
+
+        private MicaController? _micaController;
+        private SystemBackdropConfiguration? _backdropConfig;
+        private bool _dragRegionRegistered = false;
 
         public MainWindow()
         {
             this.InitializeComponent();
             this.Title = "HUDRA Control Center";
+
             LayoutRoot.DataContext = this;
+            TrySetMicaBackdrop();
+            SetInitialSize();
+            MakeBorderlessWithRoundedCorners();
+        }
+
+       
+
+        private void SetInitialSize()
+        {
+            IntPtr hwnd = WindowNative.GetWindowHandle(this);
+            var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
+            var appWindow = AppWindow.GetFromWindowId(windowId);
+
+            appWindow.Resize(new Windows.Graphics.SizeInt32(300, 400));
+        }
+
+        private void MakeBorderlessWithRoundedCorners()
+        {
+            IntPtr hwnd = WindowNative.GetWindowHandle(this);
+            var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
+            var appWindow = AppWindow.GetFromWindowId(windowId);
+
+            // Remove title bar and border
+            appWindow.SetPresenter(AppWindowPresenterKind.Overlapped);
+
+            var presenter = appWindow.Presenter as OverlappedPresenter;
+            if (presenter != null)
+            {
+                presenter.IsResizable = false;
+                presenter.IsMaximizable = false;
+                presenter.IsMinimizable = false;
+                presenter.SetBorderAndTitleBar(false, false);
+            }
+
+            // Optional: enable default OS-level rounded corners (Windows 11 only)
+            appWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+
+            DispatcherQueue.TryEnqueue(() =>
+            {
+                var width = (int)LayoutRoot.ActualWidth;
+                var height = (int)LayoutRoot.ActualHeight;
+                System.Diagnostics.Debug.WriteLine($"LayoutRoot: {width}x{height}");
+
+                if (width > 0 && height > 0)
+                {
+                    appWindow.TitleBar.SetDragRectangles(new[]
+                    {
+            new Windows.Graphics.RectInt32
+            {
+                X = 0,
+                Y = 0,
+                Width = width,
+                Height = height
+            }
+        });
+                }
+            });
+
+        }
+
+        private bool _dragRegionSet = false;
+
+        private bool TrySetMicaBackdrop()
+        {
+            if (MicaController.IsSupported())
+            {
+                _backdropConfig = new SystemBackdropConfiguration
+                {
+                    IsInputActive = true,
+                    Theme = SystemBackdropTheme.Default
+                };
+
+                _micaController = new MicaController();
+                _micaController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+                _micaController.SetSystemBackdropConfiguration(_backdropConfig);
+
+                return true;
+            }
+
+            return false;
         }
 
         private async void SetTdpButton_Click(object sender, RoutedEventArgs e)
         {
-            // 1. Create an instance of your new service
             var tdpService = new TDPService();
-
-            // 2. Get the value from the UI and convert watts to milliwatts
             int targetTdp = (int)TdpSlider.Value;
             int tdpInMilliwatts = targetTdp * 1000;
 
-            // 3. Call the service and capture the result tuple
             var result = tdpService.SetTdp(tdpInMilliwatts);
 
-            // 4. Update display text to show new TDP
             if (result.Success)
-            {
                 CurrentTdpDisplayText = $"Current TDP: {targetTdp}W";
-            }
             else
-            {
-                // You can also use this to display errors!
                 CurrentTdpDisplayText = $"Error: {result.Message}";
-            }
         }
     }
 }
